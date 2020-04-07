51a80150cafdb9b0f31608e9602b6799b31f7daa
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2020-04-07 19:18:48 -0400

    updated example

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 (() => {
     // stub
-    let toDoList = document.querySelector("todo-list"),
+    let toDoList = document.querySelector(".todo-list"),
         addButton = document.getElementById("addToDo"),
         saveButton = document.getElementById("saveToDo");
 
     function addToDo() {
         // add a todo to the list
-        debugger;
+        // debugger;
+        let newToDo = `
+        <li>
+            <span contenteditable>Add your todo info here</span>
+            <button>Done</button>
+        </li>
+        `;
+
+        toDoList.innerHTML += newToDo; 
     }
 
     function saveToDo() {
         // save our todos to LocalStorage
-        debugger;
+        // debugger;
+        let allMyToDos = toDoList.innerHTML;
+        if(window.localStorage){
+            localStorage.setItem("myToDoList", allMyToDos);
+        }
+    }
+
+    function retrieveToDoList() {
+        if(window.localStorage){
+            if(localStorage.getItem("myToDoList")){
+                let theToDoList = localStorage.getItem("myToDoList");
+                toDoList.innerHTML = theToDoList;
+            }
+        }
     }
 
     addButton.addEventListener("click", addToDo);
     saveButton.addEventListener("click", saveToDo);
+    retrieveToDoList();
 })();
