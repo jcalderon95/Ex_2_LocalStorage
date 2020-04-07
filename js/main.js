28a47919d59e19ef1e3758eda1f788fa2ba1ac5f
Jimmy Calderon <JimmyCalderon@Jimmys-MacBook-Pro.local>
2020-04-07 18:13:04 -0400

    start

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(() => {
+    // stub
+    let toDoList = document.querySelector("todo-list"),
+        addButton = document.getElementById("addToDo"),
+        saveButton = document.getElementById("saveToDo");
+
+    function addToDo() {
+        // add a todo to the list
+        debugger;
+    }
+
+    function saveToDo() {
+        // save our todos to LocalStorage
+        debugger;
+    }
+
+    addButton.addEventListener("click", addToDo);
+    saveButton.addEventListener("click", saveToDo);
+})();
